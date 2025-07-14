f4b1bc83aed9dfcc47d99c067203791197911741
Your Name <your-email@example.com>
2025-07-14 20:27:50 +0530

    "first change "

---
+++ footer.js
@@ -0,0 +1,95 @@
+document.write(` <footer class="site-footer footer-large footer-light	footer-wide">
+
+            <!-- FOOTER BLOCKES START -->
+            <div class="footer-top overlay-wraper">
+                <div class="overlay-main"></div>
+                <div class="container">
+
+                    <div class="m-b10">
+                        <div class="wt-divider bg-gray-dark"></div>
+                    </div>
+                    <div class="row">
+                        <!-- ABOUT COMPANY -->
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="widget widget_about">
+                                <!--<h4 class="widget-title">About Company</h4>-->
+                                <div class="logo-footer clearfix p-b15">
+                                    <a href="index.html"><img src="images/logo-dark.png" alt=""></a>
+                                </div>
+                                <p class="max-w400">Hotel Midtown offers modern comfort, central location, excellent service, stylish rooms, and convenient access to top city attractions.</p>
+
+                                <ul class="social-icons social-tooltips-outer  wt-social-links">
+                                    <li><a href="javascript:void(0);" class="fa fa-facebook"><span
+                                                class="social-tooltips">Facebook</span></a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-rss"><span
+                                                class="social-tooltips">Rss</span></a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-linkedin"><span
+                                                class="social-tooltips">Linkedin</span></a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-google-plus"><span
+                                                class="social-tooltips">Google Plus</span></a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-instagram"><span
+                                                class="social-tooltips">Instagram</span></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="widget widget_services inline-links">
+                                <h4 class="widget-title">Useful links</h4>
+                                <ul>
+                                    <li><a href="about-1.html">About</a></li>
+                                    <li><a href="post-gallery.html">Gallery</a></li>
+                                    <li><a href="news-grid.html">Blog</a></li>
+                                    <li><a href="work-masonry.html">Portfolio</a></li>
+                                    <li><a href="project-detail.html">Contact Us</a></li>
+                                    <li><a href="contact-1.html">FAQ </a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="widget widget_services inline-links">
+                                <h4 class="widget-title">Rooms & Suites</h4>
+                                <ul>
+                                    <li><a href="project-detail.html">Standard Double Room (Non- AC)</a></li>
+                                    <li><a href="project-detail.html">Double Deluxe Room (Non- AC)</a></li>
+                                    <li><a href="project-detail.html">Double Deluxe Room (AC)</a></li>
+                                    <li><a href="project-detail.html">Triple Deluxe Room ( AC)</a></li>
+                                    <li><a href="project-detail.html">Dirmatory</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="widget widget_address_outer">
+                                <h4 class="widget-title">Contact Us</h4>
+                                <ul class="widget_address">
+                                    <li><i class="sl-icon-map site-text-primary"></i>Near Lord shiv temple, NH - 59, Main road, Daringbadi, Kandhamal, Odisha, PIN - 762104 </li>
+                                    <li><i class="sl-icon-envolope-letter site-text-primary"></i>hotelmidtowndaringbadi1@gmail.com
+                                    </li>
+                                    <li><i class="sl-icon-phone site-text-primary"></i>(+91) 9337335835</li>
+                                </ul>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+            <!-- FOOTER COPYRIGHT -->
+            <div class="footer-bottom overlay-wraper">
+                <div class="overlay-main"></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="wt-footer-bot-center">
+                            <span class="copyrights-text">© 2024 Your Company. Designed By Thewebmax.</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+        
+        `);
---
+++ header.js
@@ -0,0 +1,194 @@
+document.write(`
+    <header class="site-header header-style-1 mobile-sider-drawer-menu">
+            <div class="sticky-header main-bar-wraper">
+                <div class="main-bar p-t5">
+                    <div class="container">
+                        <div class="logo-header">
+                            <div class="logo-header-inner logo-header-one">
+                                <a href="index.html">
+                                    <img src="images/logo-light.png" alt="" />
+                                </a>
+                            </div>
+                        </div>
+                        <!-- NAV Toggle Button -->
+                        <button id="mobile-side-drawer" data-target=".header-nav" data-toggle="collapse" type="button"
+                            class="navbar-toggler collapsed">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar icon-bar-first"></span>
+                            <span class="icon-bar icon-bar-two"></span>
+                            <span class="icon-bar icon-bar-three"></span>
+                        </button>
+                        <!-- ETRA Nav -->
+
+                        <!-- MAIN Vav -->
+                        <div class="header-nav navbar-collapse collapse">
+                            <ul class=" nav navbar-nav">
+                                <li class="active">
+                                    <a href="index.html">Home</a>
+                                </li>
+                                <li>
+                                    <a href="room-detail.html">Rooms</a>
+                                    
+                                </li>
+                                <li>
+                                    <a href="post-gallery.html">Gallery</a>
+                                    
+                                </li>
+                                <li class="submenu-direction">
+                                    <a href="about-1.html">About Us</a>
+                                    
+                                </li>
+                                <li class="submenu-direction">
+                                    <a href="contact-1.html">Contact Us</a>
+                                    <!-- <ul class="sub-menu">
+                                        <li><a href="accordian.html">Accordian</a></li>
+                                        <li><a href="button.html">Button</a></li>
+                                        <li><a href="icon_box.html">Icon box style</a></li>
+                                        <li><a href="list_group.html">List group</a></li>
+                                        <li><a href="modal_popup.html">Modal popup</a></li>
+                                        <li><a href="tabs.html">Tabs</a></li>
+                                        <li><a href="table.html">Table</a></li>
+                                        <li><a href="video.html">Video </a></li>
+                                        <li><a href="icon-font.html">Icon Font </a></li>
+                                    </ul> -->
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="extra-nav">
+                            <div class="extra-cell">
+                                <a href="#" class="contact-slide-show text-white"><i class="fa fa-envelope-o"></i></a>
+                            </div>
+                            <div class="extra-cell">
+                                <a href="javascript:;" class="socialicon_show  text-white">
+                                    <i class="fa fa-share-alt"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <!-- ETRA Nav -->
+
+                        <!-- Social Nav -->
+                        <div class="social_hide">
+                            <div class="side-social-nav">
+                                <a href="javascript:void(0)" class="socialicon_close">&times;</a>
+                                <ul class="list-unstyled">
+                                    <li><a href="javascript:void(0);" class="fa fa-facebook"><span
+                                                class="social-tooltip">Facebook</span></a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-rss"><span
+                                                class="social-tooltip">Rss</span></a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-linkedin"><span
+                                                class="social-tooltip">Linkedin</span></a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-google-plus"><span
+                                                class="social-tooltip">Google Plus</span></a></li>
+                                    <li><a href="javascript:void(0);" class="fa fa-instagram"><span
+                                                class="social-tooltip">Instagram</span></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <!-- Contact Nav -->
+                        <div class="contact-slide-hide">
+                            <div class="contact-nav">
+                                <a href="javascript:void(0)" class="contact_close">&times;</a>
+                                <div class="contact-nav-form p-a30">
+                                    <form class="cons-contact-form" method="post" action="form-handler.php">
+                                        <div class="contact-one m-b30">
+
+                                            <!-- TITLE START -->
+                                            <div class="section-head text-center">
+                                                <h2 class="m-b5" data-title="Form">Get In Touch</h2>
+                                            </div>
+                                            <!-- TITLE END -->
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group wt-inputicon-box">
+                                                        <input name="username" type="text" required class="form-control"
+                                                            placeholder="Name">
+                                                        <i class="fs-input-icon sl-icon-user"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group wt-inputicon-box">
+                                                        <input name="email" type="text" class="form-control" required
+                                                            placeholder="Email">
+                                                        <i class="fs-input-icon sl-icon-envolope-letter"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <div class="form-group wt-inputicon-box">
+                                                        <textarea name="message" rows="3" class="form-control " required
+                                                            placeholder="Message"></textarea>
+                                                        <i class="fs-input-icon sl-icon-envolope"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <div class="text-center">
+                                                        <button name="submit" type="submit" value="Submit"
+                                                            class="btn-half site-button button-lg m-b15">
+                                                            <span>Submit</span><em></em>
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <div class="contact-info text-black m-b30">
+                                        <!-- TITLE START -->
+                                        <div class="section-head text-center">
+                                            <h2 class="m-b5" data-title="Info">Contact Info</h2>
+                                        </div>
+                                        <!-- TITLE END -->
+                                        <ul>
+                                            <li>
+                                                <div class="wt-icon-box-wraper center p-b40">
+                                                    <div class="icon-md m-b20"><i class="sl-icon-phone"></i></div>
+                                                    <div class="icon-content">
+                                                        <h5 class="m-t0 font-weight-500">Phone number</h5>
+                                                        <p>+91 9337335835</p>
+                                                    </div>
+                                                </div>
+                                            </li>
+
+                                            <li>
+                                                <div class="wt-icon-box-wraper center p-b40">
+                                                    <div class="icon-md m-b20"><i class="sl-icon-envolope-letter"></i>
+                                                    </div>
+                                                    <div class="icon-content">
+                                                        <h5 class="m-t0 font-weight-500">Email address</h5>
+                                                        <p>hotelmidtowndaringbadi1@gmail.com</p>
+                                                    </div>
+                                                </div>
+                                            </li>
+
+                                            <li>
+                                                <div class="wt-icon-box-wraper center">
+                                                    <div class="icon-md m-b20"><i class="sl-icon-map"></i></div>
+                                                    <div class="icon-content">
+                                                        <h5 class="m-t0 font-weight-500">Address info</h5>
+                                                        <p>Near Lord shiv temple, NH - 59, Main road, Daringbadi, Kandhamal, Odisha, 762104</p>
+                                                    </div>
+                                                </div>
+                                            </li>
+
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- SITE Search -->
+                        <div id="search">
+                            <span class="close"></span>
+                            <form role="search" id="searchform" action="/search" method="get" class="radius-xl">
+                                <div class="input-group">
+                                    <input value="" name="q" type="search" placeholder="Type to search" />
+                                    <span class="input-group-btn"><button type="button" class="search-btn"><i
+                                                class="fa fa-search"></i></button></span>
+                                </div>
+                            </form>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </header>`);
